e6bde3f648048808b701f840e7dd297f16f6837e
Sunil Kr. Singh <sunil.singh@economical.com>
2022-09-17 22:04:52 -0400

    Corrected code of ReminderActivityTest checked in - import of R had a problem

--- starter/app/src/main/java/com/udacity/project4/locationreminders/geofence/GeofenceBroadcastReceiver.kt
+++ starter/app/src/main/java/com/udacity/project4/locationreminders/geofence/GeofenceBroadcastReceiver.kt
@@ -3,6 +3,7 @@ package com.udacity.project4.locationreminders.geofence
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 
 /**
  * Triggered by the Geofence.  Since we can have many Geofences at once, we pull the request
@@ -16,9 +17,10 @@ import android.content.Intent
 
 class GeofenceBroadcastReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
-
+        Log.i("GeofenceReceiver", "Before calling enqueueWork")
 //TODO: implement the onReceive method to receive the geofencing events at the background
         GeofenceTransitionsJobIntentService.enqueueWork(context, intent)
+        Log.i("GeofenceReceiver", "Before calling enqueueWork")
 
     }
 }
